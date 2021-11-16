a350f090567fddd36115fb494fc7d81869d26f3f
ellendejong <de.jong.ellen@hotmail.com>
2021-11-16 17:23:02 +0100

    use test_data.

--- tests/test_create_workflow_reference_files.py
+++ tests/test_create_workflow_reference_files.py
@@ -1,51 +1,32 @@
 #! venv/bin/python
 # standard libraries alphabetic order of main package.
 import configparser
+import csv
 import shutil
+import pandas as pd
 
 # third party libraries alphabetic order of main package.
+import requests
 import pytest
 
 # local libraries alphabetic order of main package.
 import assets.create_workflow_reference_files as create_ref
 
-@pytest.fixture(scope="module", autouse=True)
-def setup_and_get_test_path(tmp_path_factory):
-    test_tmp_path = str(tmp_path_factory.mktemp("data")) + "/"
-
-    # create empty config
-    open(test_tmp_path + "empty.ini", "a").close()
-    open(test_tmp_path + "empty_translation.csv", "a").close()
-    # create config missing key
-    config = configparser.ConfigParser()
-    config['DEFAULT'] = {
-        'species': 'homo sapiens',
-        'translation_table': 'fake.csv',
-    }
-    with open(test_tmp_path + 'missing_key.ini', 'w') as config_missing_key:
-        config.write(config_missing_key)
-
-    # create config non existing translation file
-    config['DEFAULT']['ensembl_url'] = "url"
-    with open(test_tmp_path + 'non_existing_translation_file.ini', 'w') as config_no_tf:
-        config.write(config_no_tf)
-    
-    # create config empty translation file
-    config['DEFAULT']['translation_table'] = test_tmp_path + "empty_translation.csv"
-    with open(test_tmp_path + 'empty_translation_file.ini', 'w') as config_empty_tf:
-        config.write(config_empty_tf)
-
-    shutil.copy("./assets/create_workflow_reference_files.ini", test_tmp_path)
-    return test_tmp_path
 
 # TODO implement these tests.
-'''
-rsID not exist in ensembl
-rsID not linked to gene in ensembl
+"""
 rsID and/or location linked to multiple genes -> not unique for single genotype gene.
-variant ensembl query no result
-variant with multiple alternative alleles
-'''
+variant with multiple alternative alleles ensembl
+variant with multiple alternative alleles translation table
+"""
+
+
+@pytest.fixture(scope="module")
+def create_test_files(tmp_path_factory):
+    test_path = str(tmp_path_factory.mktemp("tests")) + "/"
+    shutil.rmtree(test_path)
+    shutil.copytree("tests/test_data/", test_path)
+    return test_path
 
 class TestCreateRefsParser():
     def test_parser_optional_config_section(self):
@@ -56,13 +37,13 @@ class TestCreateRefsParser():
     
     def test_parser_optional_bed(self):
         parser = create_ref.parse_arguments_and_check(
-            args_in=["--bed", "./references/sites_of_interest.bed"]
+            args_in=["--bed", "references/sites_of_interest.bed"]
         )
         assert parser
 
     def test_parser_optional_output_path(self):
         parser = create_ref.parse_arguments_and_check(
-            args_in=["--output_path", "./references/"]
+            args_in=["--output_path", "references/"]
         )
         assert parser
 
@@ -74,7 +55,7 @@ class TestCreateRefsParser():
 
     def test_parser_optional_yaml(self):
         parser = create_ref.parse_arguments_and_check(
-            args_in=["--yaml", "./references/sites_of_interest.yaml"]
+            args_in=["--yaml", "references/sites_of_interest.yaml"]
         )
         assert parser
 
@@ -87,7 +68,7 @@ class TestCreateRefsParser():
     def test_parser_optional_output_path_not_exist(self):
         with pytest.raises(FileNotFoundError):
             parser = create_ref.parse_arguments_and_check(
-                args_in=["--output_path", "./non_existing_path/"]
+                args_in=["--output_path", "non_existing_path/"]
             )
 
     def test_parser_optional_yaml_not_exist(self):
@@ -104,46 +85,39 @@ class TestCreateRefsConfig():
                 section="DEFAULT", config_file="non_existing_file.ini"
             )
 
-    def test_config_section_not_exists(self, setup_and_get_test_path):
+    def test_config_section_not_exists(self, create_test_files):
         with pytest.raises(KeyError):
             config_section = create_ref.read_config_section_and_check(
-                section="non_existing_section", config_file=setup_and_get_test_path + "create_workflow_reference_files.ini"
+                section="non_existing_section", config_file=create_test_files + "/ini_config/correct_config.ini"
             )
 
-    def test_config_key_not_exists(self, setup_and_get_test_path):
+    def test_config_key_not_exists(self, create_test_files):
         with pytest.raises(KeyError):
             config_section = create_ref.read_config_section_and_check(
-                section="DEFAULT", config_file=setup_and_get_test_path + "missing_key.ini"
+                section="DEFAULT", config_file=create_test_files + "/ini_config/missing_key.ini"
             )
 
-    def test_config_file_empty(self, setup_and_get_test_path):
+    def test_config_file_empty(self, create_test_files):
         with pytest.raises(OSError) as empty_error:
             config_section = create_ref.read_config_section_and_check(
-                section="DEFAULT", config_file=setup_and_get_test_path + "empty.ini"
+                section="DEFAULT", config_file=create_test_files + "/ini_config/empty.ini"
             )
         assert "empty" in str(empty_error.value)
     
-    def test_config_translation_empty(self, setup_and_get_test_path):
+    def test_config_translation_empty(self, create_test_files):
         with pytest.raises(OSError) as empty_error:
             config_section = create_ref.read_config_section_and_check(
-                section="DEFAULT", config_file=setup_and_get_test_path + "empty_translation_file.ini"
+                section="DEFAULT", config_file=create_test_files + "/ini_config/empty_translation_file.ini"
             )
         assert "empty" in str(empty_error.value)
 
-    def test_config_translation_not_exists(self, setup_and_get_test_path):
+    def test_config_translation_not_exists(self, create_test_files):
         with pytest.raises(FileNotFoundError):
             config_section = create_ref.read_config_section_and_check(
-                section="DEFAULT", config_file=setup_and_get_test_path + "non_existing_translation_file.ini"
+                section="DEFAULT", config_file=create_test_files + "/ini_config/non_existing_translation_file.ini"
             )
 
 
-# TODO implement these tests in TestCreateRefsTranslationFile.
-'''
-not all rename columns present.
-genotype col does not contain : or /
-genotype id not present in all rows.
-gene_and_rs_id values not contain _
-'''
 class TestCreateRefsTranslationFile():
     pass
 
