10920f7b9c52d76f0f205524d9b9af10974a106f
ellendejong <de.jong.ellen@hotmail.com>
2021-11-18 11:54:57 +0100

    migrate pytest requirements to setup.py. Removed conftest.py

--- setup.py
+++ setup.py
@@ -4,4 +4,16 @@ setup(
     name='assets',
     version='0.0.1',
     packages=find_packages(include=['assets']),
+    install_requires=[
+        "deepdiff == 5.6.0",
+        "natsort == 7.1.1",
+        "numpy == 1.21.2",
+        "ordered-set == 4.0.2",
+        "pandas == 1.3.3",
+        "pysam == 0.17.0",
+        "pyvcf == 0.6.8",
+        "pyyaml == 5.4.1",
+    ],
+    setup_requires=['pytest-reqs==0.2.1', 'pytest==6.2.5'],
+    tests_require=['pytest-reqs==0.2.1', 'pytest==6.2.5'],
 )
--- tests/conftest.py
+++
@@ -1,9 +0,0 @@
-import os
-import pysam
-import pytest
-from pytest_reqs import check_requirements
-import shutil
-# @pytest.fixture(scope="session")
-# def tmpdir(tmp_path_factory):
-#     test_tmp_path = tmp_path_factory.mktemp("data")
-#     return test_tmp_path
