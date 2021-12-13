52a8911aa2b49222b5f4bdb07facf07238c2e259
ellendejong <de.jong.ellen@hotmail.com>
2021-12-13 10:52:32 +0100

    add requests requirement

--- setup.py
+++ setup.py
@@ -1,19 +1,13 @@
 from setuptools import setup, find_packages
 
+with open("./assets/requirements.txt") as requirements_file:
+    requirements = requirements_file.readlines()
+
 setup(
     name='assets',
     version='0.0.1',
     packages=find_packages(include=['assets']),
-    install_requires=[
-        "deepdiff == 5.6.0",
-        "natsort == 7.1.1",
-        "numpy == 1.21.2",
-        "ordered-set == 4.0.2",
-        "pandas == 1.3.3",
-        "pysam == 0.17.0",
-        "pyvcf == 0.6.8",
-        "pyyaml == 5.4.1",
-    ],
+    install_requires=requirements,
     setup_requires=['pytest-reqs==0.2.1', 'pytest==6.2.5'],
     tests_require=['pytest-reqs==0.2.1', 'pytest==6.2.5'],
 )
