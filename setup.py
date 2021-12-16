eedb336e5cf5634d6afb8c1d985bbab38cc1fcd6
ellendejong <de.jong.ellen@hotmail.com>
2021-12-16 13:56:17 +0100

    requirements to requirements.txt

--- setup.py
+++ setup.py
@@ -8,6 +8,4 @@ setup(
     version='0.0.1',
     packages=find_packages(include=['assets']),
     install_requires=requirements,
-    setup_requires=['pytest-reqs>=0.2.1', 'pytest>=6.2.5', 'pytest-flake8>=1.0.7', 'flake8<=4.0.0'],
-    tests_require=['pytest-reqs>=0.2.1', 'pytest>=6.2.5', 'pytest-flake8>=1.0.7', 'flake8<=4.0.0'],
 )
