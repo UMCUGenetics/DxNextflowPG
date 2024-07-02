f1556e67bf311c126c29433f5afac14363ce9eb6
Jorisvansteenbrugge <7196110+Jorisvansteenbrugge@users.noreply.github.com>
2024-07-02 09:24:45 +0200

    run script updated

--- run_DxNextflowPG.sh
+++ run_DxNextflowPG.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 set -euo pipefail
 
-workflow_path='/hpc/diaggen/software/production/DxNextflowPG/'
+workflow_path='/home/cog/jvansteenbrugge/workdir/pypgx/DxNextflowPG/'
 
 # Set input and output dirs
 input=`realpath -e $1`
 output=`realpath $2`
 email=$3
-optional_params=( "${@:4}" )
-mkdir -p $output && cd $output
-mkdir -p log
+bam_path=$4
+genome=$5
+optional_params=( "${@:6}" )
 
-if ! { [ -f 'workflow.running' ] || [ -f 'workflow.done' ] || [ -f 'workflow.failed' ]; }; then
-touch workflow.running
 
 sbatch <<EOT
 #!/bin/bash
-#SBATCH --time=24:00:00
+#SBATCH --time=06:00:00
 #SBATCH --nodes=1
 #SBATCH --mem 5G
 #SBATCH --gres=tmpspace:10G
@@ -28,46 +26,18 @@ sbatch <<EOT
 #SBATCH --export=NONE
 #SBATCH --account=diaggen
 
-export NXF_JAVA_HOME='/hpc/diaggen/software/tools/jdk-18.0.2.1/'
+export NXF_JAVA_HOME='/hpc/diaggen/users/joris/software/tools/java/jdk'
 
-/hpc/diaggen/software/production/DxNextflowPG/tools/nextflow run $workflow_path \
---input $input \
---outdir $output \
---email $email \
--resume -ansi-log false \
-${optional_params[@]:-""}
+/hpc/diaggen/users/joris/software/tools/nextflow/nextflow run $workflow_path \
+    --input $input \
+    --outdir $output \
+    --email $email \
+    --bam_path $bam_path \
+    --genome_fasta $genome \
+    -resume \
+    -ansi-log false \
+    -profile slurm \
+    ${optional_params[@]:-""}
 
-if [ \$? -eq 0 ]; then
-    echo "Nextflow done."
 
-    echo "Zip work directory"
-    find work -type f | egrep "\.(command|exitcode)" | zip -@ -q work.zip
-
-    echo "Remove work directory"
-    rm -r work
-
-    echo "Creating md5sum"
-    find -type f -not -iname 'md5sum.txt' -exec md5sum {} \; > md5sum.txt
-
-    echo "PG workflow completed successfully."
-    rm workflow.running
-    touch workflow.done
-
-    echo "Change permissions"
-    chmod 770 -R $output
-
-    exit 0
-else
-    echo "Nextflow failed"
-    rm workflow.running
-    touch workflow.failed
-
-    echo "Change permissions"
-    chmod 775 -R $output
-
-    exit 1
-fi
 EOT
-else
-echo "Workflow job not submitted, please check $output for 'workflow.status' files."
-fi
