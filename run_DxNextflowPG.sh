6870208f1292c2144a92b5c989c46b7dd8a771d0
Jorisvansteenbrugge <7196110+Jorisvansteenbrugge@users.noreply.github.com>
2024-07-03 08:34:41 +0200

    included missing option in the runscript

--- run_DxNextflowPG.sh
+++ run_DxNextflowPG.sh
@@ -4,13 +4,14 @@ set -euo pipefail
 workflow_path='/home/cog/jvansteenbrugge/workdir/pypgx/DxNextflowPG/'
 
 # Set input and output dirs
-input=`realpath -e $1`
+input=`realpath -e $1` # is currently ignored since already mapped bam files are used
 output=`realpath $2`
-email=$3
-bam_path=$4
-genome=$5
-optional_params=( "${@:6}" )
-
+email=`realpath $3`
+bam_path=`realpath $4`
+genome=`realpath $5`
+pypgx_resources=`realpath $6`
+optional_params=( "${@:7}" )
+workdir=`pwd`
 
 sbatch <<EOT
 #!/bin/bash
@@ -27,13 +28,14 @@ sbatch <<EOT
 #SBATCH --account=diaggen
 
 export NXF_JAVA_HOME='/hpc/diaggen/users/joris/software/tools/java/jdk'
-
+cd $workdir
 /hpc/diaggen/users/joris/software/tools/nextflow/nextflow run $workflow_path \
     --input $input \
     --outdir $output \
     --email $email \
     --bam_path $bam_path \
     --genome_fasta $genome \
+    --pypgx_resource_bundle $pypgx_resources \
     -resume \
     -ansi-log false \
     -profile slurm \
